4e69c32429ca07e4f509132f5ee349010d02d2f4
Badmus Deji <swiftexpro@gmail.com>
2024-06-12 23:04:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+// import TodoForm from './Component/TodoForm'
+import TodoList from './Component/TodoList'
+
+
+ import './App.css'
+
+function App() {
+  return (
+  
+    <div className="App">
+    
+    <TodoList/>
+    
+    </div>
+    
+
+  )
+}
+
+export default App
